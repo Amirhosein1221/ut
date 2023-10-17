1cb12905b71230aca0a8b176cd0c9ba4ab4795fc
Amirhosein1221 <113098258+Amirhosein1221@users.noreply.github.com>
2023-10-17 05:03:18 -0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,8 @@
+function like() {
+    document.getElementById('like').style.backgroundColor = 'green';
+    document.getElementById('dlike').style.backgroundColor = 'rgba(255,255,255,.25)';
+}
+function dlike() {
+    document.getElementById('dlike').style.backgroundColor = 'red';
+    document.getElementById('like').style.backgroundColor = 'rgba(255,255,255,.25)';
+}
